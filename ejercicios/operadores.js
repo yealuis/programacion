a2ce6e98b7ea9f0e4ab22fa30c1d77accdedf8af
yealuis <luisjavierperez1999@gmail.com>
2024-11-14 12:21:48 -0400

    ejercios

---
+++ ejercicios/operadores.js
@@ -0,0 +1,116 @@
+// operadores aritmeticos
+/*let suma = 1 + 1
+let resta = 2 - 1
+let multi = 2 * 2
+let divi = 6 / 2
+let modulo = 10 % 2
+let concat = 'bla ' + 'bla'
+
+console.log(suma)
+console.log(resta)
+console.log(multi)
+console.log(divi)
+console.log(modulo)
+console.log(concat)
+
+// operadores de comparacion
+let menorQue = 20 < 50
+let mayorQue = 60 > 20
+let igual = 100 == 100
+let menorIgual = 20 <= 50
+let mayorIgual = 60 >= 20
+let desigual = 20 != 20
+
+console.log(menorQue)
+console.log(mayorQue)
+console.log(igual)
+console.log(menorIgual)
+console.log(mayorIgual)
+console.log(desigual)
+
+// operadores logicos
+let and = true && true
+let or = true || false
+
+console.log(and)
+console.log(or)
+
+let andPrimera = 10 == 10 && 40 > 10
+let andSegundo = 50 >= 25 && 20 < 100
+let andTercera = 35 != 19 && 27 <= 7
+
+let orPrimera = 10 == 10 || 40 > 10
+let orSegundo = 50 >= 25 || 20 < 100
+let orTercera = 35 != 35 || 27 <= 1
+
+console.log(andPrimera)
+console.log(andSegundo)
+console.log(andTercera)
+console.log(orPrimera)
+console.log(orSegundo)
+console.log(orTercera)
+
+let fruta = 'durazno'
+
+if (fruta == "manzana" || fruta == "durazno") {
+    console.log("Adivinaste")
+} else {
+    console.log("No adivinaste")
+}
+
+let dia = 'domingo'
+
+if (dia == 'sabado') {
+    console.log('Puedes ir a la piscina')
+} else if (dia == 'domingo') {
+    console.log('puedes descansar')
+} else {
+    console.log('Tienes que ir a trabajar')
+}
+
+//Funciones
+function saludo(nombre, apellido) {
+    return 'Hola ' + nombre + ' ' + apellido
+}
+console.log(saludo('Luis', 'perez'))
+
+function suma(num1, num2) {
+    return num1 + num2
+}
+console.log(suma(10,20))
+
+function mayor(numUno, numDos, numTres) {
+    if (numUno >= numDos && numUno >= numTres) {
+        return numUno + ' Es mayor'
+    } else if (numDos >= numUno && numDos >= numTres) {
+        return numDos + ' Es mayor'
+    } else if (numTres >= numDos && numTres >= numUno) {
+        return numTres + ' Es mayor'
+    }
+}
+console.log(mayor(10,30,50))
+
+function parImpar(numero) {
+    if (numero % 2 == 0) {
+        return 'Es par'
+    } else {
+        return 'Es impar'
+    }
+}
+console.log(parImpar(119))
+
+function mayorEdad(edad, compa) {
+    return edad >= 18 || (edad >= 15 && compa == true)
+}
+console.log(mayorEdad(20, false))
+
+// Ciclos
+for (let i = 1; i < 16; i++) {
+    console.log(i)
+}
+*/
+// Arreglos
+
+let array = ['luis', 324, true]
+
+console.log(array)
