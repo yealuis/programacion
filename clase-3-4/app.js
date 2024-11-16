c8f6e29f54e250c2daeac15b33c80f2b5ab22f38
yealuis <luisjavierperez@gmail.com>
2024-11-16 10:12:22 -0400

    notas

--- clase-3-4/app.js
+++ clase-3-4/app.js
@@ -1,9 +1,36 @@
-let nota = process.argv[2]
+const yargs = require('yargs')
 
-if (nota == 'add') {
-    console.log('Nota agregada')
-} else if (nota == 'remove') {
-    console.log('Nota removida')
-} else {
-    console.log('Comandos disponibles: add , remove')
-}
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a note',
+    handler: function() {
+        console.log('Nota agregada!')
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Nota removida!')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Nota')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List of all notes',
+    handler: function() {
+        console.log('Lista de todas las notas')
+    }
+})
+
+yargs.parse()
