8a5e0167096ed92eca607752d624daaf1e84b52f
yealuis <luisjavierperez@gmail.com>
2024-11-30 12:05:59 -0400

    new file:   clase-5/app.js 	new file:   clase-5/index.html

---
+++ clase-5/app.js
@@ -0,0 +1,8 @@
+//let title = document.querySelector('#title')
+//console.log(title)
+
+let para = document.getElementById('para')
+para.innerText = 'Lo que sea, bla blsa bla'
+
+let title = document.getElementsByClassName('title')
+console.log(title)
